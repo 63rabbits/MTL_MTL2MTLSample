173a366c8f5a4e629a1b1031e01fbe9ef0cfab44
63rabbits goodman <63rabbitsgoodman@gmail.com>
2023-08-13 19:19:48 +0900

    Specify midTexture storage mode as private.

--- MTL_MTL2MTLSample/ViewController.swift
+++ MTL_MTL2MTLSample/ViewController.swift
@@ -72,6 +72,7 @@ class ViewController: UIViewController, MTKViewDelegate {
                                         height: texture.height,
                                         mipmapped: false)
         midTextureDescriptor.usage = [ .renderTarget, .shaderRead ]
+        midTextureDescriptor.storageMode = .private
         midTexture = device.makeTexture(descriptor: midTextureDescriptor)
     }
     
